e0aca7483d5f767880c24a26a4c6c388556705dc
Dheeraj Yadav <yadavdheeraj417@gmail.com>
2021-03-09 17:44:01 +0530

    Change in UI and image URls

--- src/app/common/material/material.module.ts
+++ src/app/common/material/material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
 
 const modules = [
   MatCardModule,
-  MatButtonModule
+  MatButtonModule,
+  MatIconModule
 ];
 
 @NgModule({
